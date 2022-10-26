473491c519ed4ed9453f54f27fd413af419c22e1
Mutabazi Christian <c.mutabazi@alustudent.com>
2022-10-26 13:47:03 +0200

    correcting the first task

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,2 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-    print(number +"is positive")
-elif number == 0:
-    print(number +"is zero")
-else:
-    print(number +"is negative")
+#!/bin/bash
+python3 -c $PYCODE
