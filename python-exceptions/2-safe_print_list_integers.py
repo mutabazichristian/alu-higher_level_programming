7cb90361ba57730160db01e2d27229c84b80671c
Mutabazi Christian <c.mutabazi@alustudent.com>
2022-11-09 15:15:52 +0200

    task 2 try 10

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,14 +2,14 @@
 # function that prints first x elements of a list and only intergers
 
 
-def safe_print_list_integers(my_list=[],x=0):
+def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     try:
         for i in range(x):
             if type(my_list[i]) is int:
-             print("{:d}".format(my_list[i]),end="")
+             print("{:d}".format(my_list[i]), end="")
              counter+=1
-    except (ValueError,TypeError):
+    except (ValueError, TypeError):
         pass
     print("")
     return counter
