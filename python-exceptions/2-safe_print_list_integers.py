ba0ba34e6e6ff167a464b54c37e2f6af7504e22f
Mutabazi Christian <c.mutabazi@alustudent.com>
2022-11-09 15:17:11 +0200

    task 2 try 11

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
             if type(my_list[i]) is int:
-             print("{:d}".format(my_list[i]), end="")
-             counter+=1
+                print("{:d}".format(my_list[i]), end="")
+                counter+= 1
     except (ValueError, TypeError):
         pass
     print("")
