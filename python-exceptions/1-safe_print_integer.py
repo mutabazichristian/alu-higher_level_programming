d8f283e1c55e120f96b43e74b3fedabfa72e3ab8
Mutabazi Christian <c.mutabazi@alustudent.com>
2022-11-09 14:25:30 +0200

    task 1 try 7

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 # function that prints an integer with "{:d}".format()
-
 def safe_print_integer(value):
         try:
             print("{:d}".format(value))
-            return(True)
+            return True
         except:
-            return(False)
-
+            return False
