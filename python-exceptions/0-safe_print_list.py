5dc79ab069c2fd18c49a58049b271be9745465e5
Mutabazi Christian <c.mutabazi@alustudent.com>
2022-11-09 13:33:18 +0200

    task 0 try 5

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 #function that prints x elements
 
 
-def safe_print_list(my_list=[0,"one",2,"three",4,"five"], x=3):
+def safe_print_list(my_list=[], x=0):
     for i in my_list:
         try:
             print (i)
@@ -10,4 +10,4 @@ def safe_print_list(my_list=[0,"one",2,"three",4,"five"], x=3):
             break
         if i == my_list[x] :
             break
-safe_print_list(x=1)
+        return (x)
