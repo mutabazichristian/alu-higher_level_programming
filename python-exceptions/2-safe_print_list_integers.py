975386568cc003e04ebc1c90ef5ed5e8caf94c39
Mutabazi Christian <c.mutabazi@alustudent.com>
2022-11-09 14:53:20 +0200

    task 2 try 4

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,9 +6,11 @@ def safe_print_list_integers(my_list=[],x=0):
     counter = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]),end=" ")
+            print("{:d}".format(my_list[i]),end="")
             counter+=1
         except IndexError:
             break
-        print(" ")
+    print(" ")
+    print(counter)
     return counter
+
