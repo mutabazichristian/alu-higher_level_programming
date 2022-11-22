1876ed2c365ee9abf1b659f867383e496f768439
Mutabazi <113415988+mutabazichristian@users.noreply.github.com>
2022-11-22 15:29:15 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 import urllib.request
 with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
     html = response.read()
-    print('Body response: ')
+    print('Body response:')
     print('\t- type: {}'.format(type(html)))
     print('\t- content: {}'.format(html))
     print('\t- utf8 content: {}'.format(html.decode("utf-8")))
