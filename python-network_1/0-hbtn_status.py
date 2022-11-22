a8baa6ca3bfade0e8510bb1805db0360ec6d3a73
Mutabazi Christian <c.mutabazi@alustudent.com>
2022-11-22 14:10:48 +0200

    correcting file naming

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+'''documentation and stuff'''
+
 import urllib.request
 with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
     html = response.read()
