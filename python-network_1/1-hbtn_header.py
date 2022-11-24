0d1c364cd644df4b5dd28d30a548cd0cde73c4ae
Mutabazi Christian <c.mutabazi@alustudent.com>
2022-11-24 16:02:15 +0200

    correcting task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Script that takes in URL, sends a request to the URL and displays the value of X-Request-Id '''
 
-import urllib
+import urllib.request
 import sys
 
 '''too many documentations'''
