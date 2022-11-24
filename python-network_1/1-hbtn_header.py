1caf65c109f321b6874f68330dfdff0dbc0bd0e7
Mutabazi Christian <c.mutabazi@alustudent.com>
2022-11-24 16:04:58 +0200

    add in task 10

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''Script that takes in URL, sends a request to the URL and displays the value of X-Request-Id '''
+'''Script that takes in URL,
+    sends a request to the URL 
+    and displays the value of X-Request-Id '''
 
 import urllib.request
 import sys
