b99bd7700b50b0c8fb0c80ef4063773aa03275f7
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-05 09:32:16 +0100

    task 7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const string = 'C is fun';
+const argv = process.argv.slice();
+const x = argv[2];
+
+if (isNaN(+x)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i > +x; i++) {
+    console.log(string);
+  }
+}
