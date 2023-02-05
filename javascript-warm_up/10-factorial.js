eb54cc1ffce3582e60d5b47d2459c431a5fad863
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-05 10:31:29 +0100

    task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,9 +2,9 @@
 const argv = process.argv.slice();
 const firstInteger = +argv[2];
 
-function factorial (num) {
+function factorial(num) {
   let fac = 1;
-  for(let i = 1; i <= num; i++){
+  for (let i = 1; i <= num; i++) {
     fac = fac * i;
   }
   return fac;
