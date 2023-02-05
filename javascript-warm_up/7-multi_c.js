756b5dfba5f1c38748e3398f8c57ff1fcd9d87aa
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-05 09:34:09 +0100

    task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -6,7 +6,7 @@ const x = argv[2];
 if (isNaN(+x)) {
   console.log('Missing number of occurences');
 } else {
-  for (let i = 0; i > +x; i++) {
+  for (let i = 0; i < +x; i++) {
     console.log(string);
   }
 }
