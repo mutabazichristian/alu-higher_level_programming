be93b1908e8c4314f193a6f1798569b8f2d2ecb1
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-05 09:43:13 +0100

    task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,8 +5,8 @@ const x = argv[2];
 if (isNaN(x)) {
   console.log('Missing size');
 } else {
-  for(let i = 0; i < x; i++){
-    for(let j = 0; j < x; j++){
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
       console.log('x');
     }
   }
