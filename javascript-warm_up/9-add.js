07bc009cabcb2adf19e0e66e6a31729459b270c0
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-05 10:13:52 +0100

    task 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv.slice();
-const first_integer = argv[2];
-const second_integer = argv[3];
+const first_integer = +argv[2];
+const second_integer = +argv[3];
 
 
 function add(a, b) {
