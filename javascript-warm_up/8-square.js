d2548a9310c98fd78ec87140d59b73ce09e241e9
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-05 10:03:30 +0100

    task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -6,8 +6,6 @@ if (isNaN(x)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < x; i++) {
-    for (let j = 0; j < x; j++) {
-      console.log('x');
-    }
+    console.log('x'.repeat(x))
   }
 }
