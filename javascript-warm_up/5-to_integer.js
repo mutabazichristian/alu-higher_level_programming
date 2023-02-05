b2929f60def6dfb586bbb0cfaa13e185c9eb6e19
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-05 09:20:31 +0100

    task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 const argv = process.argv.slice();
 const number = argv[2];
 
-if (isNaN(+number)){
+if (isNaN(+number)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' +number);
+  console.log('My number: ' + +number);
 }
