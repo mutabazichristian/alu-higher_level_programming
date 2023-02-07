452f7fc9cd8ee2744515410b8f7581e19f66e7a7
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-07 10:56:50 +0100

    task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+let argv = +process.argv.slice(2);
+
+function findSecondLargest(argv) {
+  argv.sort((a, b) => {
+    if (a < b) return 1;
+    if (a > b) return -1;
+    return 0;
+  })
+  return argv[1];
+}
+
+if (argv.length === 1) {
+  console.log(0);
+} else if (argv.length === 0) {
+  console.log(0);
+} else {
+  console.log(findSecondLargest(argv));
+}
