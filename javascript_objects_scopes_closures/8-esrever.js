bca21984934e63ff6a59c5061a3e5244f308d130
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-09 10:38:14 +0100

    task 8

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+module.exports.esrever = function (list) {
+  const reverseList = list;
+  let count = 0;
+  for (let i = (list.length - 1); i >= 0; i--) {
+    reverseList[count] = list[i];
+    count++;
+  }
+};
