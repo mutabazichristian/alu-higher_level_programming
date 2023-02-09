497e77e2faa7f33f54d17606a1f49509cecd3f90
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-09 13:22:44 +0100

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const ask = require('request');
-const episodeId= process.argv[2];
+const episodeId = process.argv[2];
 const urlFilms = `https://swapi-api.alx-tools.com/api/films/${episodeId}`;
 
 ask.get(urlFilms, (err, res) => {
   if (err) console.log(err);
   else {
-    content = JSON.parse(res.body);
+    const content = JSON.parse(res.body);
     console.log(content.title);
   }
 });
