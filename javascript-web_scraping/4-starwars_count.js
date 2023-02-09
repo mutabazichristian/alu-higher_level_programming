863862affe920d5dcf1ad8938623ac112a31169d
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-09 13:44:21 +0100

    task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,11 +3,17 @@
 const ask = require('request');
 const url = process.argv[2];
 const characterId = 18;
+let localCount = 0;
 
 ask.get(url, (err, res) => {
   if (err) console.log(err);
   else{
-    const content = JSON.parse(res.body);
-    console.log(content);
+    const content = JSON.parse(res.body).results;
+    content.forEach(objects => {
+      objects.forEach( char=>{
+        if (char.includes('/18/')) localCount++;
+      })
+    });
   }
-})
+  console.log(localCount);
+});
