88bbb165e24940540c21cdcb274e2cc891e6ce48
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-09 09:17:24 +0100

    task 5

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,36 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    // exchanging height and width values without a third variable
+    this.height = this.height + this.width;
+    this.width = this.height - this.width;
+    this.height = this.height - this.width;
+  }
+
+  double () {
+    // doubling the value of height and width
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    this.super(size,size);
+  }
+}
+
+module.exports = Rectangle, Square;
