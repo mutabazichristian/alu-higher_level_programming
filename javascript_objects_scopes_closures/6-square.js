19e500ccf03db479a280ec18b2976081e24b8107
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-09 10:22:13 +0100

    task 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 const A = require('./5-square')
 class Square extends A {
-  constructor(size) {
-    super(size);
-  }
-
   charPrint(c) {
     if (c == undefined) {
       c = 'X';
     }
     for(let i = 0; i <this.height; i++){
-      console.log(c.repeat(this.width));
+      console.log(String(c).repeat(this.width));
     }
   }
 }
