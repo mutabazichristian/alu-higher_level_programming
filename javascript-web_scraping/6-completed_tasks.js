0ab01f23cbee9b0c172af57275f0ac02d860c5d6
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-10 09:57:40 +0100

    task 5

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//script that gets content of a webpage and stores it in a file
+const fs = require('fs');
+const ask = require('request');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+ask.require(url, (err, res) => {
+  if (err) console.log(err);
+  const data = JSON.parse(res.body);
+  fs.writeFile(filePath, data, (err) => {
+    console.log(err);
+  });
+});
