7068d5d16c9814c212bdd506bf1ae2d146bd0ef1
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-10 10:03:48 +0100

    task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//script that gets content of a webpage and stores it in a file
+// script that gets content of a webpage and stores it in a file
 
 const fs = require('fs');
 const ask = require('request');
