0b3f473f88ec84caef125c2eab0340b84e074d33
mutabazichristian <c.mutabazi@alustudent.com>
2023-02-14 09:15:04 +0100

    extra task 1

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+//script that prints all characters of a star wars movie
+
+const ask = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films${movieId}`;
+
+ask.get(url, (err, res) => {
+  if (err) console.log(err);
+  console.log(JSON.parse(res));
+})
